ae9f2b42fda820f04262e8384d46ef751f76327a
Omolemo Lethuloe <olethuloe@gmail.com>
2021-08-03 12:56:32 +0200

    Updated home page

--- modules/module1.js
+++ modules/module1.js
@@ -7,6 +7,8 @@
 
 // parent nodes
 const landingPage = document.getElementById("landingPage");
+const myBody = document.querySelector("body");
+const myScript = document.querySelector("script");
 
 // creating DOM elements
 let corner1 = document.createElement("div");
@@ -23,6 +25,30 @@ let signUp = document.createElement("button");
 let logIn = document.createElement("button");
 let logo = document.createElement("i");
 
+let middleSection = document.createElement("article");
+let aboutBookList = document.createElement("h1");
+let straightLine = document.createElement("div");
+let aboutSection = document.createElement("div");
+let aboutSectionPicture = document.createElement("div");
+let aboutSectionWriting = document.createElement("div");
+
+let quoteSection = document.createElement("section");
+let sectionQuote = document.createElement("h1");
+let sectionQuote2 = document.createElement("h1");
+let sectionQuote3 = document.createElement("h1");
+let sectionQuote4 = document.createElement("p");
+
+let footer = document.createElement("footer");
+let footerPicture = document.createElement("div");
+let footerBottom = document.createElement("div");
+let subscription = document.createElement("div");
+let joinUs = document.createElement("p");
+let joinForm = document.createElement("form");
+let joinInput = document.createElement("input");
+let joinButton = document.createElement("button");
+let lineBreaks = document.createElement("br");
+
+
 // adding classList attributes for styling
 corner1.classList = "corner1";
 corner2.classList = "corner2";
@@ -38,6 +64,36 @@ signUp.classList = "signUp";
 logIn.classList = "logIn";
 logo.classList = "fas fa-book-open";
 
+middleSection.classList = "middleSection";
+aboutBookList.classList = "aboutBookList";
+straightLine.classList = "straightLine";
+aboutSection.classList = "aboutSection";
+aboutSectionPicture.classList = "aboutSectionPicture";
+aboutSectionWriting.classList = "aboutSectionWriting";
+
+quoteSection.classList = "quote";
+sectionQuote.classList = "sectionQuote";
+sectionQuote2.classList = "sectionQuote2";
+sectionQuote3.classList = "sectionQuote3";
+sectionQuote4.classList = "sectionQuote4";
+
+footer.classList = "footer";
+footerPicture.classList = "footerPicture";
+footerBottom.classList = "footerBottom";
+subscription.classList = "subscription";
+joinUs.classList = "joinUs";
+joinForm.classList = "joinForm";
+joinInput.classList = "joinInput";
+joinButton.classList = "joinButton";
+
+// adding attributes 
+joinInput.setAttribute("type","text");
+joinInput.setAttribute("required","");
+joinInput.setAttribute("placeholder","Email*");
+joinButton.setAttribute("type","submit");
+
+
+
 // assigning text to element variables
 heading.textContent = "My";
 span.textContent = "Book"; 
@@ -47,6 +103,18 @@ headingLastPart.textContent = "List.";
 
 signUp.textContent = "SignUp";
 logIn.textContent = "LogIn";
+
+aboutBookList.textContent = "About MyBookList";
+aboutSectionWriting.textContent = "Plan what to read next. \n Keep track of what you've read. \n Read more books.";
+
+sectionQuote.textContent = "\"";
+sectionQuote2.textContent= "MY PROBLEM WITH READING BOOKS IS THAT I GET DISTRACTED.";
+sectionQuote3.textContent = "BY OTHER BOOKS.";
+sectionQuote4.textContent = "/ Every reader."
+
+footerBottom.textContent = "© 2021 by MyBookList. Proudly created by Omolemo & Rethabile.";
+joinUs.textContent = "JoinUs!";
+joinButton.textContent = "Submit"
 
 // appending elements to parent div
 nav.appendChild(logo);
@@ -61,6 +129,34 @@ banner.appendChild(description);
 banner.appendChild(listButton);
 landingPage.appendChild(banner);
 banner.appendChild(corner1);
+
+myBody.insertBefore(middleSection,myScript);
+middleSection.appendChild(aboutBookList);
+middleSection.appendChild(straightLine);
+
+aboutSection.appendChild(aboutSectionPicture);
+aboutSection.appendChild(aboutSectionWriting);
+middleSection.appendChild(aboutSection);
+
+myBody.insertBefore(quoteSection,myScript);
+quoteSection.appendChild(sectionQuote);
+quoteSection.appendChild(sectionQuote2);
+quoteSection.appendChild(sectionQuote3);
+quoteSection.appendChild(sectionQuote4);
+
+myBody.insertBefore(footer,myScript);
+footer.appendChild(footerPicture);
+footer.appendChild(footerBottom);
+
+subscription.appendChild(joinUs);
+subscription.appendChild(joinForm);
+joinForm.appendChild(joinInput);
+joinForm.appendChild(lineBreaks);
+joinForm.appendChild(joinButton);
+footerPicture.appendChild(subscription);
+
+
+
  
 
 
